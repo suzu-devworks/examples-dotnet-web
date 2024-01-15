e960e58d1c6f0db0a22be1bb931c15ff825cd1c6
A.suzuki <suzu.devworks@gmail.com>
2024-01-15 14:37:04 +0000

    Add Authentication example to Auth.Identity.

--- src/Examples.Web.Authentication.Identity/Controllers/HomeController.cs
+++ src/Examples.Web.Authentication.Identity/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Examples.Web.Authentication.Identity.Models;
 
@@ -18,6 +19,7 @@ public class HomeController : Controller
         return View();
     }
 
+    [Authorize]
     public IActionResult Privacy()
     {
         return View();
